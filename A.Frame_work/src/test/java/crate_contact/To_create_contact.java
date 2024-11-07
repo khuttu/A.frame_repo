a5a177f7847a5168ace6bfa3023ad6edd16e0aea
Adarsh <Adarsh@DESKTOP-5FE5K1T>
2024-11-07 20:25:53 +0530

    updated to_crate_org & to_create_contact , added pink

--- A.Frame_work/src/test/java/crate_contact/To_create_contact.java
+++ A.Frame_work/src/test/java/crate_contact/To_create_contact.java
@@ -3,8 +3,9 @@ package crate_contact;
 public class To_create_contact {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("new contact crate");
+		System.out.println("mob no");
 	}
 
 }
---
+++ A.Frame_work/src/test/java/pink/Colour.java
@@ -0,0 +1,10 @@
+package pink;
+
+public class Colour {
+
+	public static void main(String[] args) {
+
+		System.out.println("this for pinky's pink pen");
+	}
+
+}
